a81c921c216e70d63ad2249f6aba803634ca2f61
Julio Spinelli <julio.spinelli@dataprev.gov.br>
2021-04-14 11:27:19 -0300

    Situação após a prática 5

--- labs/practices/ProductManagement/src/main/java/labs/pm/app/Shop.java
+++ labs/practices/ProductManagement/src/main/java/labs/pm/app/Shop.java
@@ -2,6 +2,7 @@ package labs.pm.app;
 
 import java.math.BigDecimal;
 import labs.pm.data.Product;
+import labs.pm.data.Rating;
 
 /**
  * {@code Shop} class represents an application that manages Products
@@ -11,10 +12,21 @@ import labs.pm.data.Product;
  */
 public class Shop {
     public static void main(String[] args) {
-        Product p1 = new Product();
-        p1.setId(101);
-        p1.setName("Tea");
-        p1.setPrice(BigDecimal.valueOf(1.99));
-        System.out.println(p1.getId() + " " + p1.getName() + " " + p1.getPrice() + " " + p1.getDiscount());
+        Product p1 = new Product(101, "Tea", BigDecimal.valueOf(1.99));
+        Product p2 = new Product(102, "Cofee", BigDecimal.valueOf(1.99), Rating.FOUR_STAR);
+        Product p3 = new Product(103, "Cake", BigDecimal.valueOf(3.99), Rating.FIVE_STAR);
+        Product p4 = new Product();
+        Product p5 = p3.applyRating(Rating.THREE_STAR);
+
+        printProduct(p1);
+        printProduct(p2);
+        printProduct(p3);
+        printProduct(p4);
+        printProduct(p5);
+    }
+
+    private static void printProduct(Product product) {
+        System.out.println(product.getId() + " " + product.getName() + " " + product.getPrice() + " " + product.getDiscount() + " "
+                + product.getRating().getStars());
     }
 }
--- labs/practices/ProductManagement/src/main/java/labs/pm/data/Product.java
+++ labs/practices/ProductManagement/src/main/java/labs/pm/data/Product.java
@@ -1,6 +1,7 @@
 package labs.pm.data;
 
 import static java.math.RoundingMode.HALF_UP;
+import static labs.pm.data.Rating.*;
 
 import java.math.BigDecimal;
 
@@ -25,19 +26,28 @@ public class Product {
     private int id;
     private String name;
     private BigDecimal price;
+    private Rating rating;
+
+    public Product() {
+        this(0, "No name", BigDecimal.ZERO);
+    }
+
+    public Product(int id, String name, BigDecimal price, Rating rating) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.rating = rating;
+    }
+
+    public Product(int id, String name, BigDecimal price) {
+        this(id, name, price, NOT_RATED);
+    }
 
     /**
      * @return the id
      */
     public int getId() {
         return id;
-    }
-
-    /**
-     * @param id the id to set
-     */
-    public void setId(final int id) {
-        this.id = id;
     }
 
     /**
@@ -48,13 +58,6 @@ public class Product {
     }
 
     /**
-     * @param name the name to set
-     */
-    public void setName(final String name) {
-        this.name = name;
-    }
-
-    /**
      * @return the price
      */
     public BigDecimal getPrice() {
@@ -62,10 +65,10 @@ public class Product {
     }
 
     /**
-     * @param price the price to set
+     * @return the rating
      */
-    public void setPrice(final BigDecimal price) {
-        this.price = price;
+    public Rating getRating() {
+        return rating;
     }
 
     /**
@@ -76,5 +79,9 @@ public class Product {
      */
     public BigDecimal getDiscount() {
         return this.price.multiply(DISCOUNT_RATE).setScale(2, HALF_UP);
+    }
+
+    public Product applyRating(Rating newRating) {
+        return new Product(this.id, this.name, this.price, newRating);
     }
 }
