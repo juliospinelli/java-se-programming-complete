02780780a65e69d4b5f16033aa26d4ed78ce690f
Julio Spinelli <julio.spinelli@dataprev.gov.br>
2021-04-21 16:59:34 -0300

    Final da prática 10

--- labs/practices/ProductManagement/src/main/java/labs/pm/data/ProductManager.java
+++ labs/practices/ProductManagement/src/main/java/labs/pm/data/ProductManager.java
@@ -8,25 +8,38 @@ import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import java.util.ResourceBundle;
+import java.util.Set;
 
 public class ProductManager {
 
     private Map<Product, List<Review>> products = new HashMap<>();
-    private Locale locale;
-    private ResourceBundle resources;
-    private DateTimeFormatter dateFormat;
-    private NumberFormat moneyFormat;
+    private ResourceFormatter formatter;
+
+    private static Map<String, ResourceFormatter> formatters = Map.of("en-GB", new ResourceFormatter(Locale.UK),
+            "en-US", new ResourceFormatter(Locale.US), "fr-FR", new ResourceFormatter(Locale.FRANCE), "ru-RU",
+            new ResourceFormatter(new Locale("ru", "RU")), "pt-BR", new ResourceFormatter(new Locale("pt", "BR")),
+            "zh-CN", new ResourceFormatter(Locale.CHINA));
 
     public ProductManager(Locale locale) {
-        this.locale = locale;
-        this.resources = ResourceBundle.getBundle("labs.pm.data.resources", locale);
-        this.dateFormat = DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT).localizedBy(locale);
-        this.moneyFormat = NumberFormat.getCurrencyInstance(locale);
+        this(locale.toLanguageTag());
+    }
+
+    public ProductManager(String languageTag) {
+        changeLocale(languageTag);
+    }
+
+    public void changeLocale(String languageTag) {
+        formatter = formatters.getOrDefault(languageTag, formatters.get("pt-BR"));
+    }
+
+    public static Set<String> getSupportedLocales() {
+        return formatters.keySet();
     }
 
     public Product createProduct(int id, String name, BigDecimal price, Rating rating, LocalDate bestBefore) {
@@ -64,9 +77,7 @@ public class ProductManager {
         List<Review> reviews = products.get(product);
 
         StringBuilder txt = new StringBuilder();
-        txt.append(MessageFormat.format(resources.getString("product"), product.getName(),
-                moneyFormat.format(product.getPrice()), product.getRating().getStars(),
-                dateFormat.format(product.getBestBefore())));
+        txt.append(formatter.formatProduct(product));
         txt.append('\n');
 
         Collections.sort(reviews);
@@ -76,13 +87,12 @@ public class ProductManager {
             if (review == null) {
                 break;
             }
-            txt.append(MessageFormat.format(resources.getString("review"), review.getRating().getStars(),
-                    review.getComments()));
+            txt.append(formatter.formatReview(review));
             txt.append('\n');
         }
 
         if (reviews.isEmpty()) {
-            txt.append(resources.getString("no.reviews"));
+            txt.append(formatter.getText("no.reviews"));
             txt.append('\n');
         }
 
@@ -91,6 +101,19 @@ public class ProductManager {
 
     public void printProductReport(int id) {
         printProductReport(findProduct(id));
+    }
+
+    public void printProducts(Comparator<Product> sorter) {
+        List<Product> productList = new ArrayList<>(products.keySet());
+        productList.sort(sorter);
+
+        StringBuilder txt = new StringBuilder();
+        for (Product product : productList) {
+            txt.append(formatter.formatProduct(product));
+            txt.append('\n');
+        }
+
+        System.out.println(txt);
     }
 
     public Product findProduct(int id) {
@@ -102,5 +125,34 @@ public class ProductManager {
             }
         }
         return result;
+    }
+
+    private static class ResourceFormatter {
+        private Locale locale;
+        private ResourceBundle resources;
+        private DateTimeFormatter dateFormat;
+        private NumberFormat moneyFormat;
+
+        private ResourceFormatter(Locale locale) {
+            this.locale = locale;
+            this.resources = ResourceBundle.getBundle("labs.pm.data.resources", locale);
+            this.dateFormat = DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT).localizedBy(locale);
+            this.moneyFormat = NumberFormat.getCurrencyInstance(locale);
+        }
+
+        private String formatProduct(Product product) {
+            return MessageFormat.format(resources.getString("product"), product.getName(),
+                    moneyFormat.format(product.getPrice()), product.getRating().getStars(),
+                    dateFormat.format(product.getBestBefore()));
+        }
+
+        private String formatReview(Review review) {
+            return MessageFormat.format(resources.getString("review"), review.getRating().getStars(),
+                    review.getComments());
+        }
+
+        private String getText(String key) {
+            return resources.getString(key);
+        }
     }
 }
